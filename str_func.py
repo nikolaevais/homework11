3421508c66e1331cce9955fe2f5a82ffa84936c0
ASUS_TUF_FEO <nikolaeva_i_s@mail.ru>
2024-04-09 18:15:01 +0300

    title

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def str_upper(line):
+    """
+    меняет все буквы на заглавные
+    """
     print(line.upper())
+
+
+def str_title(line):
+    """
+    меняет первые буквы на заглавные
+    """
+    print(line.title())
